271c8ae1a11cf6fab86293793b25b84dbabb6f55
Lawrence Hudson <quicklizard@googlemail.com>
2016-07-05 12:01:50 +0100

    Fix build for CRAN

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 VERSION=`cat DESCRIPTION  | grep Version: | sed s/.*:\ //g`
 echo Building Homeric ${VERSION} using `R --version | head -1`
-# TODO check --as-cran should be run by R-devel
 rm -rf build  && \
     mkdir build && \
     cd build && \
     R CMD build --compact-vignettes=gs .. && \
-    R CMD check Homeric_${VERSION}.tar.gz && \
+    ~/local/R-devel/bin/R CMD check --as-cran Homeric_${VERSION}.tar.gz && \
     R CMD INSTALL --build --html Homeric_${VERSION}.tar.gz
